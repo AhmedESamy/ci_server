b917e692efdfe883bea0161c98983411db7ba69a
Kvas-Max <36407449+Kvas-Max@users.noreply.github.com>
2025-02-08 11:32:00 +0100

    first bare functionality

--- src/ci_server.py
+++ src/ci_server.py
@@ -1,20 +1,25 @@
 import logging
 from flask import Flask, request, jsonify
-from webhook_handler import process_webhook
-from notifier import send_notification
+import system_routines
 
 # Initialize Flask app for webhook handling
 app = Flask(__name__)
 
 # Handle incoming webhook requests
-@app.route("/webhook", methods=["POST"])
-def handle_webhook():
+@app.route("/update/", methods=["POST"])
+def updatehandler():
     """Handles incoming webhook events from GitHub and triggers necessary actions."""
-    payload = request.json
-    response = process_webhook(payload)
-    return response
+    data = request.get_json()
+    
+    
+    """Run tests"""
+    system_routines.clone_and_run(data)
+    
+    return jsonify({"message": "Received update, running tests"}), 200
+
+
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     logging.info("Starting CI server...")
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=3200, debug=True)
